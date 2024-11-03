e789f4cb0d8539ca581d634d3120fac66159ba0c
andriferry <ferryandri35@gmail.com>
2024-11-03 16:50:58 +0700

    Update Error Pages

---
+++ error.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts"></script>
+
+<template>
+    <div class="h-screen flex justify-center flex-col gap-10 items-center">
+        <div
+            class="flex text-9xl gap-3 font-bold text-slate-600 items-center justify-center">
+            <span class="icon-[noto--sad-but-relieved-face]"></span>
+            <h1 class="">404</h1>
+        </div>
+
+        <h1 class="text-2xl lg:text-3xl font-bold text-slate-700">
+            Ops, we couldn't find anything here!
+        </h1>
+
+        <NuxtLink to="/" class="btn btn-primary text-white text-lg">
+            Back to Home
+        </NuxtLink>
+    </div>
+</template>
