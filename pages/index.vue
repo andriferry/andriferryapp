9f4e85ec3eaf96d2629060206b1522ecee0a2d57
andriferry <ferryandri35@gmail.com>
2024-11-07 13:38:14 +0700

    Update prototyoe

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,19 @@
 <script setup lang="ts"></script>
 
 <template>
-   <Hero />
+    <Hero />
+
+    <div>Latest Project</div>
+
+    <div>Service</div>
+
+    <div>Testimonial</div>
+
+    <div>Resume / About me</div>
+
+    <div>Contact me</div>
+
+    <div>Blog</div>
 </template>
 
 <style scoped></style>
