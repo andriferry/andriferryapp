b18b312a917814f5e74608a85fb59481c21ac313
andriferry <ferryandri35@gmail.com>
2024-12-03 11:08:32 +0700

    Add testimonial

--- components/AboutMe.vue
+++ components/AboutMe.vue
@@ -9,6 +9,7 @@ import Next from '@/components/Icons/Next.vue';
 import Nuxt from '@/components/Icons/Nuxt.vue';
 import Pinia from '@/components/Icons/Pinia.vue';
 import React from '@/components/Icons/React.vue';
+import Slack from '@/components/Icons/Slack.vue';
 import Supabase from '@/components/Icons/Supabase.vue';
 import Tailwindcss from '@/components/Icons/Tailwindcss.vue';
 import Typescript from '@/components/Icons/Typescript.vue';
@@ -16,8 +17,9 @@ import Vite from '@/components/Icons/Vite.vue';
 import VsCode from '@/components/Icons/VsCode.vue';
 import Vue from '@/components/Icons/Vue.vue';
 import Vuetifyjs from '@/components/Icons/Vuetifyjs.vue';
-
-import Slack from '@/components/Icons/Slack.vue';
+import AnimatedCounter from 'vue-animated-counter';
+import { Carousel, Slide } from 'vue3-carousel';
+import 'vue3-carousel/dist/carousel.css';
 
 const iconsComponent = shallowRef([
     {
@@ -99,12 +101,47 @@ const myTools = shallowRef([
     },
 ]);
 
+const config = {
+    autoplay: 2000,
+    wrapAround: true,
+    pauseAutoplayOnHover: true,
+    itemsToShow: 1.5,
+    transition: 500,
+};
+
 const experienceAge = computed(() => {
     let birthday = new Date('2019-05-01');
     var ageDifMs = Date.now() - birthday.getTime();
     var ageDate = new Date(ageDifMs); // miliseconds from epoch
     return Math.abs(ageDate.getUTCFullYear() - 1970);
 });
+
+const testimonials = ref([
+    {
+        text: "He's working well with the entire team, gives frequent feedback about his progress, responds quickly, is available and easy to work with. Thanks, Andri.",
+        img: 'https://www.upwork.com/profile-portraits/c17vsiSaxPorjR2E4zzi7SxvOTdzKME0srnDaaC_tlq1Fy59iuET1C91ZxGkd3MJQE',
+        name: 'Ulrich Mann',
+        company: 'Narimo systems',
+    },
+    {
+        text: "Andri is a persistent man. He doesn't give up easily. A good-natured guy with a good work ethic. He is nearly always available. I have never seen him go offline",
+        img: 'https://www.upwork.com/profile-portraits/c1Ytdxg7i-qp0r7cfXBqzdlG4IMHKVFCQAGzBxoqjYVDcxTplBYcEJb-OHABRWrfH8',
+        name: 'Roshan Jonnalagadda',
+        company: 'Jonah and Associates Pty Ltd',
+    },
+    {
+        text: 'Great to work with and can work with Tailwind CSS within vuejs fine. We had one project here we had hired Andri for ...',
+        img: 'https://www.upwork.com/profile-portraits/c1y5663lpxcND-IA7isd0LhwRfgp-GdppGKVQqAZuz943r2xbUVudKvEF55CiivVxX',
+        name: 'Nathan Sapwell',
+        company: 'DreamFast',
+    },
+    {
+        text: 'He delivered VueJS web application project within the timeline. The communication was smooth and he met all deadlines. I enjoyed working with Andri ...',
+        img: 'https://www.upwork.com/profile-portraits/c1XkdwXmAbqmIR4L0fgLsGgJx9Wd7oU8I8Jpdh4FIhfP-b0OB2NY_gfbkgORkwdZb_',
+        name: 'Helmi Hasan',
+        company: 'PayNet',
+    },
+]);
 </script>
 
 <template>
@@ -119,14 +156,12 @@ const experienceAge = computed(() => {
                     <span class="icon-[twemoji--waving-hand]"></span>
                 </h1>
 
-                <ClientOnly>
-                    <div class="avatar">
-                        <div
-                            class="w-20 ring-gray-300 shadow-xl ring-offset-white ring ring-offset-1 rounded-full">
-                            <img src="~assets/image/profile.jpg" />
-                        </div>
+                <div class="avatar">
+                    <div
+                        class="w-20 ring-gray-300 shadow-xl ring-offset-white ring ring-offset-1 rounded-full">
+                        <img src="~assets/image/profile.jpg" />
                     </div>
-                </ClientOnly>
+                </div>
 
                 <p class="text-accent mt-0">
                     I'm Andri Ferry, I work as a remote worker, software
@@ -189,7 +224,7 @@ const experienceAge = computed(() => {
                                 <div class="stat px-0">
                                     <ClientOnly>
                                         <AnimatedCounter
-                                            :value="experienceAge"
+                                            :value="5"
                                             :duration="1000"
                                             class="mb-0 stat-value plus text-5xl font-[800] text-h1 text-primary" />
                                     </ClientOnly>
@@ -239,6 +274,44 @@ const experienceAge = computed(() => {
                         </div>
                     </div>
                 </div>
+
+                <h3 class="text-info">What Some of my Clients Say</h3>
+
+                <Carousel v-bind="config">
+                    <Slide v-for="(data, index) in testimonials" :key="index">
+                        <div class="carousel__item">
+                            <div class="card bg-zinc-50 w-96 h-56">
+                                <div class="card-body">
+                                    <article
+                                        class="prose prose-img:!m-0 prose-p:!m-0">
+                                        <p class="text-justify">
+                                            {{ data.text }}
+                                        </p>
+
+                                        <div
+                                            class="flex items-center gap-3 mt-4">
+                                            <div class="avatar">
+                                                <div
+                                                    class="mask shadow-xl mask-squircle w-10">
+                                                    <img :src="data.img" />
+                                                </div>
+                                            </div>
+
+                                            <div class="">
+                                                <p class="font-bold">
+                                                    {{ data.name }}
+                                                </p>
+                                                <p class="text-xs font-light">
+                                                    {{ data.company }}
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </article>
+                                </div>
+                            </div>
+                        </div>
+                    </Slide>
+                </Carousel>
             </article>
         </Container>
     </Wrapper>
--- plugins/animatedCounter.client.ts
+++
@@ -1,4 +0,0 @@
-import AnimatedCounter from 'vue-animated-counter';
-export default defineNuxtPlugin((nuxtApp) => {
-    nuxtApp.vueApp.component('AnimatedCounter', AnimatedCounter);
-});
