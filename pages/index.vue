271ba87932a6b264f08449c922276ba0b2507a9e
andriferry <ferryandri35@gmail.com>
2024-12-04 10:34:46 +0700

    Update Index blogs

--- pages/index.vue
+++ pages/index.vue
@@ -12,12 +12,12 @@
 
         <div class="overflow-hidden relative">
             <Container class="grid gap-4 lg:grid-cols-4">
-                <div class="col-span-2 lg:col-span-4">
+                <div class="col-span-1 lg:col-span-4">
                     <article class="prose">
                         <h3 class="text-info flex gap-3 items-center">
-                            My Skills
+                            Recent Articles
                             <span
-                                class="icon-[twemoji--man-technologist] w-6 h-6"></span>
+                                class="icon-[bi--rss-fill] w-6 h-6"></span>
                         </h3>
                     </article>
                 </div>
