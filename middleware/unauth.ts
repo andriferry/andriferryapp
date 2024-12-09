6ddff3e748cbbd1e0d6778de9929a7d00c01df3f
andriferry <ferryandri35@gmail.com>
2024-12-09 14:34:19 +0700

    Remove all Supabase

--- middleware/unauth.ts
+++
@@ -1,8 +0,0 @@
-export default defineNuxtRouteMiddleware((to, from) => {
-    const user = useSupabaseUser();
-    // Object.prototype.hasOwnProperty(user.value?.recovery_sent_at)
-
-    if (user.value) {
-        return navigateTo('/dashboard');
-    }
-});
